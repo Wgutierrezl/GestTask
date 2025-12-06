a963f5834025cbdca3be284734c386fb7f9d6036
WALTER <waltergutierrezl123@gmail.com>
2025-12-06 17:42:10 -0500

    we start to modified the swagger endpoints into the user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -37,6 +37,9 @@ const userController=new UserController();
  *               contrasena:
  *                 type: string
  *                 example: maradona
+ *               rol:
+ *                  type: string
+ *                  example: admin
  *     responses:
  *       201:
  *         description: Usuario creado
@@ -45,10 +48,80 @@ const userController=new UserController();
  */
 router.post('/registerUser',userController.createUser);
 
+
+/**
+ * @swagger
+ * /users/loginUser:
+ *   post:
+ *     summary: Iniciar sesión
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: example@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Login exitoso con token
+ *       401:
+ *         description: Credenciales inválidas
+ *       500:
+ *         description: Error al iniciar sesión
+ */
 router.post('/loginUser',userController.loginUser);
 
+
+/**
+ * @swagger
+ * /users/getUserById/{id}:
+ *   get:
+ *     summary: Obtener un usuario por ID (solo Admin)
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID del usuario
+ *     responses:
+ *       200:
+ *         description: Usuario encontrado
+ *       404:
+ *         description: Usuario no encontrado
+ *       403:
+ *         description: Rol no autorizado
+ *       500:
+ *         description: Error al obtener el usuario
+ */
 router.get('/getUserById/:id',authMiddleware(['admin','usuario']),userController.getUserById);
 
+/**
+ * @swagger
+ * /users/getAllUsers:
+ *   get:
+ *     summary: Obtener todos los usuarios (solo Admin)
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios
+ *       403:
+ *         description: Rol no autorizado
+ *       500:
+ *         description: Error al obtener usuarios
+ */
 router.get('/getAllUsers',authMiddleware(['admin']),userController.getAllUsers);
 
 export default router;
